84ba88cd4d279b04802280d814dee29b25c5e482
BabyGrootTheEpic <62159575+BabyGrootTheEpic@users.noreply.github.com>
2022-02-21 22:04:15 -0600

    Change some image links

--- server/chat-plugins/hangman.ts
+++ server/chat-plugins/hangman.ts
@@ -172,7 +172,7 @@ export class Hangman extends Rooms.RoomGame {
 	}
 
 	hangingMan() {
-		return `<img width="120" height="120" src="//${Config.routes.client}/fx/hangman${this.incorrectGuesses === -1 ? 7 : this.incorrectGuesses}.png" />`;
+		return `<img width="120" height="120" src="//raw.githubusercontent.com/BabyGrootTheEpic/Sprites/main/play.pokemonshowdown.com/fx/hangman${this.incorrectGuesses === -1 ? 7 : this.incorrectGuesses}.png" />`;
 	}
 
 	generateWindow() {
